02635cd2952eb771b226370e638edbfe00979feb
geolinkllc <geolink.llc@gmail.com>
2021-05-28 00:55:05 +0900

    π

--- lib/view/tickets_page.dart
+++ lib/view/tickets_page.dart
@@ -2,7 +2,6 @@ import 'dart:io';
 import 'dart:ui';
 
 import 'package:flutter/material.dart';
-import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
 import 'package:get/get.dart';
 import 'package:luckus7/model/order.dart';
 import 'package:luckus7/model/ticket.dart';
